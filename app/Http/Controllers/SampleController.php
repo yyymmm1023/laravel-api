9390568cea80db0f141cfc1ac7dcc581e7d950ad
Kurogoma939 <kan.sss.939@gmail.com>
2023-09-04 23:07:31 +0900

    ログサンプル

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SampleController extends Controller
 {
     public function index()
     {
-        return response()->json([
-            'code' => '200',
-            'message' => 'Hello World!',
-        ]);
+        try{
+            $dataList = [
+                [
+                    'id' => 1,
+                    'name' => '山田太郎',
+                    'email' => 'yamada.taro@gmail.com',
+                ],
+            ];
+            Log::info('データの取得に成功しました。');
+            Log::info($dataList);
+            return response()->json($dataList, 200);
+        } catch (\Exception $e) {
+            Log::error('データの取得に失敗しました。');
+            Log::error($e->getMessage());
+            return response()->json([
+                'message' => 'データの取得に失敗しました。',
+                'code' => 'E001',
+            ], 500);
+        }
     }
 }
